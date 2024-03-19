757ebe340d00192893c3d8588b304f0acefcf9bc
AndreRichartz <andrerichartz07@gmail.com>
2024-03-19 20:56:37 -0300

    model Book and Author created

--- library/admin.py
+++ library/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Category, Publisher
+from .models import Category, Publisher, Author, Book
 
 # Register your models here.
 
 admin.site.register(Category)
 
 admin.site.register(Publisher)
+
+admin.site.register(Author)
+
+admin.site.register(Book)
--- library/models.py
+++ library/models.py
@@ -8,6 +8,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.description
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
 
 class Publisher(models.Model):
@@ -16,3 +20,29 @@ class Publisher(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    amount = models.IntegerField(default=0,null =True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name="books"
+)
+    publisher = models.ForeignKey(
+        Publisher, on_delete=models.PROTECT, related_name='books'
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.amount})"
